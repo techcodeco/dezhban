00c25ca2c3673a4022d4779f65dc8581180b4772
Mohammad Afrwzeh <mafrwzh92@gmail.com>
2026-06-10 00:17:33 +0330

    fix bug in health controller in show redis connection status

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -4,10 +4,13 @@ const instanceId = parseInt(process.env.NODE_APP_INSTANCE || "0", 10);
 const basePort = parseInt(process.env.PORT || "3000", 10);
 const PORT = basePort + instanceId;
 
-export const health = () => ({
-  ok: true,
-  service: "gateway",
-  developer: "techcode",
-  redisConnected: redis.status === "ready",
-  port: PORT,
-});
+export const health = async () => {
+  let redisStatus = await redis.ping();
+  return {
+    ok: true,
+    service: "gateway",
+    developer: "techcode",
+    redisConnected: redisStatus === "PONG",
+    port: PORT,
+  };
+};
